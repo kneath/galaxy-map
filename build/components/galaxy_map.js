f641fed71d52d6b96e563484e1e4875d3781ee32
Kyle Neath <kneath@gmail.com>
2014-12-05 13:02:10 -0800

    Get some elliptical orbits going

--- build/components/galaxy_map.js
+++ build/components/galaxy_map.js
@@ -4,14 +4,91 @@ var React = require("react")
 var ReactART = require("react-art")
 
 var Circle = require("react-art/shapes/circle")
+var Group = ReactART.Group
+var Path = ReactART.Path
+var Props = React.PropTypes
+var Shape = ReactART.Shape
 var Surface = ReactART.Surface
+var Transform = ReactART.Transform
+
+var Ellipse = React.createClass({displayName: 'Ellipse',
+
+  propTypes: {
+    width: Props.number.isRequired,
+    height: Props.number.isRequired
+  },
+
+  render: function() {
+    var rx = this.props.width/2
+    var ry = this.props.height/2
+
+    var path = Path().moveTo(0, ry)
+      .arc(this.props.width, 0, rx, ry)
+      .arc(-this.props.width, 0, rx, ry)
+      .close()
+
+    return React.createElement(Shape, React.__spread({},  this.props, {d: path}))
+  }
+
+})
+
+var Orbit = React.createClass({displayName: 'Orbit',
+
+  propTypes: {
+    diameter: React.PropTypes.number.isRequired, // longest diameter
+    angularOffset: React.PropTypes.number.isRequired // in degrees
+  },
+
+  render: function() {
+    var e = 0.2 // eccentricity
+
+    var width = this.props.diameter
+    var height = ((1 - e)/(1 + e)) * width
+    var x =  250 - width/2
+    var y = 250 - height/2
+
+    var rotation = new Transform().rotate(this.props.angularOffset, width/2, height/2)
+
+    return React.createElement(Ellipse, {x: x, y: y, 
+                    width: width, height: height, 
+                    stroke: "#dddddd", strokeWidth: 1, 
+                    transform: rotation })
+  }
+})
+
+var Galaxy = {
+  starCount: 5000,
+
+  outerRadius: 13000,
+  coreRadius: 4000,
+
+  angularOffset: 0.0004,
+  coreEccentricity: 0.9,
+  edgeEccentricity: 0.9,
+
+  coreVelocity: 200,
+  edgeVelocity: 300
+
+}
 
 var GalaxyMap = React.createClass({displayName: 'GalaxyMap',
 
   render: function() {
+    var numOrbits = 20
+    var minDiameter = 100
+    var maxDiameter = 400
+    var rotation = 200
+
+    var orbits = []
+    for(var i=0; i<numOrbits; i++) {
+      var diameter = minDiameter + (maxDiameter - minDiameter) * (i/numOrbits)
+      orbits.push(React.createElement(Orbit, {diameter: diameter, angularOffset:  rotation * (i/numOrbits) }))
+    }
+
     return (
-      React.createElement(Surface, {width: 500, height: 500 }, 
-        React.createElement(Circle, {radius: 20, fill: "#dddddd"})
+      React.createElement(Surface, {width: 500, height: 500, className: "galaxy-container"}, 
+        React.createElement(Circle, {x: 250, y: 250, radius: 2, fill: "#ffffff"}), 
+        orbits 
       )
     )
   }
